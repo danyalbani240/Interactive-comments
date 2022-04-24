f44ebff8a0ca829f58fbd5d5359f9ce376d875b5
DanyalBani <mr.d.bani.d@gmail.com>
2022-04-25 01:07:36 +0430

    next moves

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//1-getting data
+//2-adding the comments on screen
+//3-adding events for reply delete and ...
 // Getting Data From The Api
 
 let data = null;
 fetch("http://localhost:3000/comments")
-	.then((res) => res.json())
-	.then((res) => (data = res));
+  .then((res) => res.json())
+  .then((res) => (data = res));
 // making Comments
