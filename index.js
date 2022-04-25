00268406300915c757f071ded663322e4d477b2a
DanyalBani <mr.d.bani.d@gmail.com>
2022-04-26 01:05:48 +0430

    reply section created

--- index.js
+++ index.js
@@ -87,6 +87,9 @@ function createCommentElement(commentData) {
     </div>
   </div>
 </div>`;
+    if (commentData.replies !== []) {
+      createReplyElements(commentData.replies);
+    }
   }
 }
 function createUserCommentElement(commentData) {
@@ -167,4 +170,173 @@ function createUserCommentElement(commentData) {
     </div>
   </div>
 </div>`;
+}
+
+function createReplyElements(repliesData) {
+  let repliesContainerElement = document.createElement("div");
+  repliesContainerElement.classList =
+    "flex flex-col comments-container border-l-2 border-gray-300 pl-5 mt-5";
+  repliesData.forEach((replyData) => {
+    if (replyData.user.username === user) {
+      let replyElement = createUserReplyElement(replyData);
+      repliesContainerElement.appendChild(replyElement);
+    } else {
+      let replyElement = document.createElement("div");
+      replyElement.classList = "flex bg-white my-2 items-center";
+      replyElement.innerHTML = `<div
+    class="bg-purple-50 w-20 h-20 ml-2 rounded hidden md:flex flex-col items-center justify-between"
+  >
+    <img src="./images/icon-plus.svg" class="cursor-pointer" alt="" />
+    <span class="text-purple-700 font-bold">${replyData.score}</span>
+    <img
+      class="cursor-pointer"
+      src="./images/icon-minus.svg"
+      alt=""
+    />
+  </div>
+  <div
+    class="bg-white px-5 flex flex-col justify-evenly ml-2 md:ml-0 h-64 mb-2 rounded md:h-40"
+  >
+    <div class="flex items-center pr-20 md:pr-0 mt-3 text-gray-500">
+      <img
+        class="w-9 h-9 mr-2"
+        src="${replyData.user.image.png}"
+        alt="${replyData.user.username}"
+      />
+
+      <span class="font-bold text-gray-900 mr-2 text-center"
+        >${replyData.user.username}</span
+      >
+
+      <span class="text-sm">${replyData.createdAt}</span>
+      <div
+        class="text-purple-700 cursor-pointer flex-1 text-right hidden md:block"
+      >
+        <img
+          src="./images/icon-reply.svg"
+          class="w-4 mr-1 inline-block align-middle"
+          alt="reply"
+        /><span class="inline-block font-bold">Reply</span>
+      </div>
+    </div>
+    <p class="mt-3 text-gray-500 md:mt-0">
+      @${replyData.replyingData} ${replyData.content}
+    </p>
+    <div class="flex justify-between mt-2 items-center md:hidden">
+      <div
+        class="bg-purple-50 w-20 h-9 rounded flex items-center justify-evenly"
+      >
+        <img
+          src="./images/icon-plus.svg"
+          class="cursor-pointer"
+          alt=""
+        />
+        <span class="text-purple-700 font-bold">12</span>
+        <img
+          class="cursor-pointer"
+          src="./images/icon-minus.svg"
+          alt=""
+        />
+      </div>
+      <div class="text-purple-700 cursor-pointer">
+        <img
+          src="./images/icon-reply.svg"
+          class="w-4 mr-1 inline-block align-middle"
+          alt="reply"
+        /><span class="inline-block font-bold">Reply</span>
+      </div>
+    </div>
+  </div>`;
+    }
+    repliesContainerElement.appendChild(replyElement);
+  });
+}
+
+function createUserReplyElement(replyData) {
+  let replyElement = document.createElement("div");
+  replyElement.classList = "flex bg-white my-2 items-center";
+  replyElement.innerHTML = `
+  <div
+              class="bg-purple-50 w-20 h-20 ml-2 rounded hidden md:flex flex-col items-center justify-between"
+            >
+              <img src="./images/icon-plus.svg" class="cursor-pointer" alt="" />
+              <span class="text-purple-700 font-bold">${replyData.score}</span>
+              <img
+                class="cursor-pointer"
+                src="./images/icon-minus.svg"
+                alt=""
+              />
+            </div>
+            <div
+              class="bg-white ml-2 md:ml-0 my-2 px-5 flex flex-col justify-evenly h-64 rounded md:h-40"
+            >
+              <div class="flex items-center md:pr-0 pr-14 mt-3 text-gray-500">
+                <img
+                  class="w-9 h-9 mr-2"
+                  src="${replyData.user.image.png}"
+                  alt="${replyData.user.username}"
+                />
+                <span class="font-bold text-gray-900 text-center mr-2"
+                  >
+                  ${replyData.user.username}</span
+                >
+                <span
+                  class="text-white bg-purple-700 mr-2 class rounded text-sm px-1 font-light"
+                  >You</span
+                >
+                <span class="text-sm">${replyData.createdAt}</span>
+
+                <div
+                  class="text-purple-700 mx-2 cursor-pointer flex-1 justify-center hidden md:flex items-center"
+                >
+                  <img
+                    class="mx-2"
+                    src="./images/icon-delete.svg"
+                    alt="delete"
+                  />
+                  <span class="text-red-600">Delete</span>
+                </div>
+                <div
+                  class="text-purple-700 cursor-pointer flex-1 justify-center hidden md:inline-flex items-center mx-2"
+                >
+                  <img class="mx-2" src="./images/icon-edit.svg" alt="edit" />
+                  <span class="text-purple-600">Edit</span>
+                </div>
+              </div>
+              <p class="mt-3 text-gray-500 md:mt-0">
+              @${replyData.replyingData} ${replyData.content}
+              </p>
+              <div class="flex justify-between mt-2 items-center md:hidden">
+                <div
+                  class="bg-purple-50 w-20 h-9 rounded flex items-center justify-evenly"
+                >
+                  <img
+                    src="./images/icon-plus.svg"
+                    class="cursor-pointer"
+                    alt=""
+                  />
+                  <span class="text-purple-700 font-bold">  ${replyData.score}</span>
+                  <img
+                    class="cursor-pointer"
+                    src="./images/icon-minus.svg"
+                    alt=""
+                  />
+                </div>
+                <div class="flex flex-1 justify-evenly">
+                  <div class="flex items-center cursor-pointer">
+                    <img
+                      class="mx-2"
+                      src="./images/icon-delete.svg"
+                      alt="delete"
+                    />
+                    <span class="text-red-600">Delete</span>
+                  </div>
+                  <div class="flex items-center cursor-pointer">
+                    <img class="mx-2" src="./images/icon-edit.svg" alt="edit" />
+                    <span class="text-purple-600">Edit</span>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+  return replyElement;
 }
