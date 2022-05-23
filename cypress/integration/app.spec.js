7c84185698b80442d99f4f71b20d888cde639c1b
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-23 09:55:11 +0430

    testing id user can add new comment

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 describe("test the whole app if it works", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3001/");
+    cy.visit("http://127.0.0.1:5500/");
   });
   it("loads the first time comments", () => {
     cy.get(".comments-container").should("exist");
+  });
+  it.only("user can add new comment", () => {
+    let typedComment = `comment ${Math.random()}`;
+    cy.get(".add-comment-input").type(typedComment);
+    cy.get("[data-test=send-comment]").click();
+    cy.contains(typedComment);
   });
 });
