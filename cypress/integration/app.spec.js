ae3fa4aeeab775f6f755106846131652e661c158
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-23 21:26:07 +0430

    it can add new reply and delete it

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -6,10 +6,23 @@ describe("test the whole app if it works", () => {
   it("loads the first time comments", () => {
     cy.get(".comments-container").should("exist");
   });
-  it.only("user can add new comment", () => {
+  it("user can add new comment", () => {
     let typedComment = `comment ${Math.random()}`;
     cy.get(".add-comment-input").type(typedComment);
     cy.get("[data-test=send-comment]").click();
     cy.contains(typedComment);
+  });
+  it("user can reply on comments", () => {
+    let replyMessage = `replied ${Math.random()}`;
+    cy.get("[data-test=reply-el]").should("exist");
+    cy.get("[data-test='reply-el']").first().click();
+    cy.get("[data-test=reply-value]").type(replyMessage);
+    cy.get("[data-test=reply-value]").next().click();
+    cy.contains(replyMessage);
+  });
+  it.only("user can delete replies ", () => {
+    cy.get('[test-delete="1"]').click();
+    cy.get(".test-delete").click();
+    cy.get('[test-delete="1"]').should("not.exist");
   });
 });
